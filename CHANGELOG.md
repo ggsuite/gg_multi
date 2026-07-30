907ed16e37e677d5556c22270be43da27363d467
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2026-07-30 22:01:06 +0200

    Finish development of version 5.12.7

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 5.12.7 - 2026-07-30
 
 ### Changed
 
--- lib/src/gg_multi_version.dart
+++ lib/src/gg_multi_version.dart
@@ -9,4 +9,4 @@
 // coverage:ignore-file
 
 /// The version of the `gg_multi` package.
-const String ggMultiVersion = '5.12.6';
+const String ggMultiVersion = '5.12.7';
